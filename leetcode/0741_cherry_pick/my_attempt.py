063030c7741236448c7ef1019524c216865281e5
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2020-01-16 15:59:51 +0100

    leetcode-0741: start

--- leetcode/0741_cherry_pick/my_attempt.py
+++ leetcode/0741_cherry_pick/my_attempt.py
@@ -2,13 +2,38 @@ from typing import List
 
 
 class Solution:
-    def cherryPickup(self, grid: List[List[int]]) -> int:
-        self.bfs(grid)
+    def cherryPickup(self, grid: List[List[int]]):
+        # self.bfs(grid)
+        self.dfs(grid)
 
     def dfs(self, grid: List[List[int]]) -> int:
-        pass
+        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+        route = []
+        row_len = len(grid[0])
+        visited = [False] * (len(grid) * row_len)
 
-    def bfs(self, grid: List[List[int]]) -> int:
+        def visit(x: int, y: int, acc: int):
+            visited[self.get_visited_index(x, y, row_len)] = True
+            if x == row_len -1 and y == len(grid) -1:
+                raise Exception("finished")
+
+            for dir_x, dir_y in directions:
+                new_x, new_y = x + dir_x, y + dir_y
+                if not (0 <= new_x < len(grid[0])) or not (0 <= new_y < len(grid)) or grid[y][x] == -1:
+                    continue
+                if visited[self.get_visited_index(new_x, new_y, row_len)]:
+                    continue
+                visit(new_x, new_y, acc + grid[y][x])
+            route.append((x, y))
+
+        try:
+            visit(0, 0, grid[0][0])
+        except:
+            print(route)
+
+        print(route)
+
+    def bfs(self, grid: List[List[int]]):
         row_len = len(grid[0])
         directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
         visited = [False] * (len(grid) * row_len)
