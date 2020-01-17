5dab95a14e9fb14328a5dd6bf8e7fc5abe685697
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2020-01-17 15:59:09 +0100

    leetcode-0741: wip

--- leetcode/0741_cherry_pick/my_attempt.py
+++ leetcode/0741_cherry_pick/my_attempt.py
@@ -12,26 +12,10 @@ class Solution:
         results1 = []
         self.solve(grid, start, target, [(0, 0, grid[0][0])], results1)
 
-        # print(f'from {target} to {start}')
-        # results2 = []
-        # self.solve(grid, target, start, [(target_x, target_y, grid[target_y][target_x])], results2)
-
         # remove 0's
         for i in range(len(results1)):
             results1[i] = sorted([x for x in results1[i] if x[2] == 1])
-        # for i in range(len(results2)):
-        #     results2[i] = sorted([x for x in results2[i] if x[2] == 1])
-        # results1 = [x for x in results1 if x[2] == 1]
-        # results2 = [x for x in results2 if x[2] == 1]
 
-        # print(f'results1')
-        # for result1_item in results1:
-        #     print(result1_item)
-        # print(f'results2')
-        # for results2_item in results2:
-        #     print(results2_item)
-
-        combs = []
         res = 0
         for i in range(len(results1)):
             results1_item1 = results1[i]
@@ -39,31 +23,19 @@ class Solution:
                 results1_item2 = results1[j]
                 if i != j:
                     res = max(sum([x[2] for x in set(results1_item1 + results1_item2)]), res)
-                    # combs.append(results1_item1 + results1_item2)
-
-        # print('combs')
-        # for comb_item in combs:
-        #     print(set(comb_item))
-
-        print(f'res: {res}')
+        return res
 
     def solve(self, grid: List[List[int]], current: Tuple[int, int], target: Tuple[int, int], path: Path, results: List[Path]):
         x, y = current
         target_x, target_y = target
-        # print(f'[{x},{y}], path: {path}')
         if x == target_x and y == target_y:
             results.append(path)
-            # score = sum([x[2] for x in path])
-            # print(f'path: {path}, score: {score}')
             return
 
         directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
         for dir_x, dir_y in directions:
             new_x, new_y = x + dir_x, y + dir_y
-            # if x == 0 and y == 0 and new_x == 0 and new_y == 1:
-            #     print(f'visited: {visited}')
             if self.is_visitable(grid, new_x, new_y, path):
-                # print(f'({x},{y}), trying new_x: {new_x}, new_y: {new_y}, score: {grid[new_y][new_x]}')
                 self.solve(grid, (new_x, new_y), target, path+[(new_x, new_y, grid[new_y][new_x])], results)
 
     def get_visited_index(self, x: int, y: int, row_len: int):
@@ -75,11 +47,9 @@ class Solution:
         if 0 > y or y >= len(grid):
             return False
         if grid[y][x] == -1:
-            # print(f'Can\'t access [{x},{y}]')
             return False
         for path_x, path_y, score in path:
             if path_x == x and path_y == y:
-                # print(f'Already visited [{x},{y}]')
                 return False
         return True
 
