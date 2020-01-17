ee4a811b8e30152e934738dbc90315f8148dbd61
Jevgeni Rumjantsev <jrumjantsev@travix.com>
2020-01-17 15:39:42 +0100

    leetcode-0741: wip

--- leetcode/0741_cherry_pick/my_attempt.py
+++ leetcode/0741_cherry_pick/my_attempt.py
@@ -4,9 +4,25 @@ Path = List[Tuple[int, int, int]]
 
 class Solution:
     def cherryPickup(self, grid: List[List[int]]):
-        target = (len(grid[0])-1, len(grid)-1)
+        start = (0, 0)
+        target_x, target_y = len(grid[0])-1, len(grid)-1
+        target = (target_x, target_y)
+
+        print(f'from {start} to {target}')
         results1 = []
-        self.solve(grid, (0, 0), target, [(0, 0, grid[0][0])], results1)
+        self.solve(grid, start, target, [(0, 0, grid[0][0])], results1)
+
+        print(f'from {target} to {start}')
+        results2 = []
+        self.solve(grid, target, start, [(target_x, target_y, grid[target_y][target_x])], results2)
+
+        print(f'results1')
+        for result1_item in results1:
+            print(result1_item)
+
+        print(f'results2')
+        for results2_item in results2:
+            print(results2_item)
 
     def solve(self, grid: List[List[int]], current: Tuple[int, int], target: Tuple[int, int], path: Path, results: List[Path]):
         x, y = current
@@ -14,8 +30,8 @@ class Solution:
         # print(f'[{x},{y}], path: {path}')
         if x == target_x and y == target_y:
             results.append(path)
-            score = sum([x[2] for x in path])
-            print(f'path: {path}, score: {score}')
+            # score = sum([x[2] for x in path])
+            # print(f'path: {path}, score: {score}')
             return
 
         directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
@@ -24,8 +40,8 @@ class Solution:
             # if x == 0 and y == 0 and new_x == 0 and new_y == 1:
             #     print(f'visited: {visited}')
             if self.is_visitable(grid, new_x, new_y, path):
-                # print(f'({x},{y}), trying newr_x: {new_x}, new_y: {new_y}')
-                self.solve(grid, (new_x, new_y), target, path+[(new_x, new_y, grid[y][x])], results)
+                # print(f'({x},{y}), trying new_x: {new_x}, new_y: {new_y}, score: {grid[new_y][new_x]}')
+                self.solve(grid, (new_x, new_y), target, path+[(new_x, new_y, grid[new_y][new_x])], results)
 
     def get_visited_index(self, x: int, y: int, row_len: int):
         return y * row_len + x
