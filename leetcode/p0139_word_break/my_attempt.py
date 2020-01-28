a3cd84248af20e1cc313ab30c2aa2e952d2dd7d0
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2020-01-28 22:03:55 +0100

    bullshit

--- leetcode/p0139_word_break/my_attempt.py
+++ leetcode/p0139_word_break/my_attempt.py
@@ -1,25 +1,33 @@
-# import re
-# [m.start() for m in re.finditer('test', 'test test test test')]
+import re
 from typing import List
 
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        def dfs(ss: str, dict1: List[str]) -> bool:
-            if len(ss) == 0:
-                return True
+        def solve(ss: str, words: List[str], seen):
+            if len(seen) >= len(s):
+                return len(set(seen)) == len(s)
 
-            for word in dict1:
-                res = dfs(ss.replace(word, ""), [x for x in dict1 if x != word])
+            for word in words:
+                newset = seen[:]
+                found_indexes = [m.start() for m in re.finditer(word, ss)]
+                for found_index in found_indexes:
+                    for index in range(len(word)):
+                        newset.append(found_index + index)
+
+                newWordDict = [x for x in words if x != word]
+                res = solve(ss, newWordDict, newset)
                 if res:
                     return True
 
             return False
 
-        return dfs(s, wordDict)
+        return solve(s, wordDict, [])
 
 
 if __name__ == "__main__":
     s = Solution()
 
-    print(s.wordBreak("cbca", ["bc","ca"]))
+    # print(s.wordBreak("leetcode", ["leet","code"]))
+    print(s.wordBreak("catsandog", ["cats","dog","sand","and","cat"]))
+    # print(s.wordBreak("cbca", ["bc","ca"]))
